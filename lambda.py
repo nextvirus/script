8d6fa685e9873c1d227f85869851ff9e5d271f2c
nextvirus <36720504+nextvirus@users.noreply.github.com>
2020-10-24 16:15:33 +0800

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -3,20 +3,25 @@ import base64
 import gzip
 import os
 import requests
+
 def lambda_handler(event, context):
     # TODO implement
     a = event["awslogs"]["data"]
     b = base64.b64decode(a)
     c = gzip.decompress(b)
     d = json.loads(c)
-    e = d['logEvents'][0]["message"].split(": ")[2]
-    f = e.strip()
-    headers = {
-    'Accept': 'application/json',
-    }
-    data = {"game":"1", "team":"1", "order":f}
-    response = requests.post('https://stats.aws.dev-null.link/proc/refund', headers=headers, data=data)
-    print(response)
-    print(data)
+    e = d['logEvents'][0]['message']
+    for i in range(len(e)):
+        f = e.split(": ")[2]
+        g = f.strip()
+        headers = {
+        'Accept': 'application/json',
+        }
+        data = {"game":"1", "team":"1", "order":g}
+        response = requests.post('https://stats.aws.dev-null.link/proc/refund', headers=headers, data=data)
+        print(response)
+        print(data)
+        print(len(e))
+
 
 #1	Klayers-python38-requests	9	arn:aws:lambda:us-east-2:770693421928:layer:Klayers-python38-requests:9
